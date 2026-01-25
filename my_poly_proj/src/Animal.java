25e41ced43de3fafa3c467a7d605643e2733ea0e
Raajneel-Ray <96514499+Raajneel-Ray@users.noreply.github.com>
2026-01-26 00:42:06 +0530

    applying abstract class

--- my_poly_proj/src/Animal.java
+++ my_poly_proj/src/Animal.java
@@ -1,4 +1,4 @@
-public class Animal {
+public abstract class Animal {
     private String name;
     private String food;
 
@@ -49,3 +49,13 @@ class Cow extends Animal {
         return "Moo";
     }
 }
+
+// Abstract methods are methods which are defined but not implemented. By defining a method, you give it a name, specify the parameters it will take, specify the access and also the return type. But the body of the method is empty. Abstract methods can only be defined as class methods. Meaning, they can't be static. Any class containting an abstract method, should be defined as an abstract class.
+
+/**
+ * public abstract static int absMeth1(String s) - This definition is not possible. It will throw a compilation error as abstract methods cannot be static
+    private abstract String absMeth2() - This definition is not possible. It will throw a compilation error as abstract methods cannot be private. The purpose of these methods are for these to be implemented in the child class. Child classes do not have access to super class's private methods.
+ */
+
+//Please note that an abstract method has to be in an abstract class, but an abstract class doesn't have to have an abstract method. When you don't want a class to be instantiated and only be inherited, you define it as an abstract class. Consider the following example. There is Animal class. There are Dog,Cat and Cow classes which inherit. There is no point in creating an instance of Animal class. In such a case, you can define Animal class as an abstract class.
+
